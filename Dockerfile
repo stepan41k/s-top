c33ea50f6681ce392ae9d86b3b7b995448953b87
stepan41k <russia.zont@gmail.com>
2026-08-04 21:20:48 +0300

    feat: create simple program and Dockefile

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM golang:1.26-alpine AS builder
+
+WORKDIR /app
+
+COPY go.mod ./
+RUN go mod download
+
+COPY . .
+
+RUN CGO_ENABLED=0 GOOS=linux go build -ldflags="-s -w" -o /app/server
+
+FROM scratch
+
+WORKDIR /
+
+COPY --from=builder /app/server /server
+
+USER nonroot:nonroot
+
+EXPOSE 8080
+
+ENTRYPOINT [ "/server" ]
--- main.go
+++ main.go
@@ -1,24 +1,56 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
-
-	"github.com/go-chi/chi"
-	sum "github.com/stepan41k/GoTesting/task-10/sum"
-	display "github.com/stepan41k/GoTesting/task-5/display"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
+	mux := http.NewServeMux()
 
-	router := chi.NewRouter()
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 
-	router.Post("/user", display.DisplayName)
-	router.Post("/positive-sum", sum.MultiplyInteger())
+	mux.HandleFunc("/work", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Обработка запроса начата...")
+		time.Sleep(2 * time.Second)
+		fmt.Fprintln(w, "Работа завершена!")
+		log.Println("Обработка запроса завершена")
+	})
 
-	server := http.Server{
-		Addr: "localhost:8080",
-		Handler: router,
+	server := &http.Server{
+		Addr: ":8080",
+		Handler: mux,
 	}
 
-	server.ListenAndServe()
+	stop := make(chan os.Signal, 1)
+
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		log.Println("Сервер запущен на порту :8080")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Ошибка запуска сервера: %v\n", err)
+		}
+	}()
+
+	<-stop
+	log.Println("Поулчен сигнал остановки. Graceful shutdown.")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30 * time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Принудительная остановка сервера: %v\n", err)
+	}
+
+	log.Println("Сервер успешно и плавно остановлен")
 }
